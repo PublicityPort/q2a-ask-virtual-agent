1d3f0fa0f986ed8e2ce77a400ab8e82ecd94b40b
Mayur Shah <mail@mayurshah.in>
2017-09-29 15:45:54 +0530

    created files

--- qa-plugin.php
+++ qa-plugin.php
@@ -9,6 +9,6 @@ if (!defined('QA_VERSION')) {
 // Register file to override the functions
 // present in the file
 */
-qa_register_plugin_overrides('qa-upload-override.php');
+qa_register_plugin_overrides('qa-ask-override.php');
 
-qa_register_plugin_module('module', 'qa-gcs-admin.php', 'qa_gcs_admin_config', 'Q2A Google Cloud Storage plugin');
+//qa_register_plugin_module('module', 'qa-gcs-admin.php', 'qa_gcs_admin_config', 'Q2A Google Cloud Storage plugin');
