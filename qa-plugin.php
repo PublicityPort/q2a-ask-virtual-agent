e9e930710f3e0ba4920d177b3968a8f3619feb97
Mayur Shah <mail@mayurshah.in>
2017-09-30 19:56:48 +0530

    commiting files

--- qa-plugin.php
+++ qa-plugin.php
@@ -13,4 +13,6 @@ if (!defined('QA_VERSION')) {
 
 qa_register_plugin_layer('qa-va-ask-layer.php', 'q2a Ask VA Layer Layer');
 
+//qa_register_plugin_layer('search-override.php', 'GSearch Layer');
+
 qa_register_plugin_module('module', 'qa-ask-va-admin.php', 'qa_ask_va_admin_config', 'Q2A Ask Virtual Agent plugin');
--- search-override.php
+++ search-override.php
@@ -1,6 +1,9 @@
 <?php
 
-function process_search($query, $start, $count, $userid, $absoluteurls, $fullcontent){
-    
-    return process_search_base($query, $start, $count, $userid, $absoluteurls, $fullcontent);
+class qa_search_basic_layer extends qa_search_basic
+{
+    function process_search($query, $start, $count, $userid, $absoluteurls, $fullcontent){
+        $query .= 'query ++';
+        return process_search_base($query, $start, $count, $userid, $absoluteurls, $fullcontent);
+    }
 }
